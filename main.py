688b66316d7329f4e6f51740b5b622ffe6d5bae2
Guilherme <guilhermebauer16@gmail.com>
2023-06-13 18:48:47 -0300

    Add banco de dados

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import sqlite3
+from pathlib import Path
+
+
+
+connection = sqlite3.connect('reservasHotel.db')
+cursor = connection.cursor()
+
+
 pessoa = str(input("Por favor imforme seu nome: "))
