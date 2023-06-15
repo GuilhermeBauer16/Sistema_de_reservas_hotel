3da3dc76991123bb2e1e0056af97749183ef59a4
Guilherme <guilhermebauer16@gmail.com>
2023-06-15 17:53:16 -0300

    ADD sistema de quartos

--- functions.py
+++ functions.py
@@ -5,25 +5,40 @@ def titulo(msg, quant=40):
     print('=' * quantia)
 
 
-def conversor_numero(msg,type):
+def conversor_numero(msg,tipo = int):
 
     while True:
         opcao = input(msg)
         try:
-            num_opcao = type(msg)
+            num_opcao = tipo(opcao)
             break
         except ValueError:
             print('\033[31mPor favor selecione um número\033[m')
-
+    return num_opcao
 
 def quartos():
     titulo('imformações sobre os quartos')
     print('''
     [1]Quarto simples
-    [2]Quanto para 2 pessoas
+    [2]Quanto duplo
     [3]Quarto familia
     [4]Quarto de luxo
     [5]Suite presidencial
+    [6]sair 
     ''')
     print('=' * 80)
     opcao = conversor_numero('Imforme o quarto que deseja saber mais: ',int)
+    if opcao == 1: 
+        titulo('Quarto simples')
+        print("""O quarto simples é uma opção aconchegante e econômica para uma pessoa. 
+Ele oferece uma cama confortável, um espaço tranquilo e os itens essenciais 
+para uma estadia confortável.""")
+        print()
+        print('valor R$ 100,00 a diária ')
+    
+    elif opcao == 2:
+        titulo('Quarto duplo')
+        print("""O quarto duplo é ideal para casais ou amigos
+que desejam compartilhar acomodações. Esse quarto oferece uma 
+ou duas camas de casal, garantindo um descanso tranquilo para ambos os hóspedes.
+ Além disso, possui comodidades básicas para atender às necessidades de duas pessoas.""")
--- main.py
+++ main.py
@@ -3,17 +3,40 @@ import functions
 connection = pymysql.connect(
 
     host='localhost',
+    port= 3306, 
     user='root',
     password='0910',
-    database='reservas_Hotel'
+    database='reservas_hotel'
 
 )
 cursor = connection.cursor()
 
+# cursor.execute(""" CREATE TABLE reservas (
+#     id INT NOT NULL AUTO_IMCREMENT PRIMARY KEY 
+#     usuario "
+# )
+# """)
 functions.titulo('Reservas')
 print("""
 [1]Imformações quartos 
 [2]clientes """)
+
+opção = functions.conversor_numero('Sua opção: ' , int)
+
+while True:
+
+    if opção == 1:
+        functions.quartos()
+    
+    elif opção == 2:
+        while True:
+            functions.titulo('Usuarios', )
+            print("""
+[1]Novo usuario
+[2]Usuarios 
+[3]Voltar ao menu
+        """
+              )
 
 #SQL
 cursor.close()
